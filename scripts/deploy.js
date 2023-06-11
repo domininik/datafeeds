9b6f6b159312c548e5242bc9bd17a2655ac60bba
domininik <dominik.fijas+github@gmail.com>
2023-06-11 12:05:03 +0200

    Fix deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,28 +4,26 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require("hardhat")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const DECIMALS = "18"
+  const INITIAL_VALUE = "200000000000000000000"
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const Feed = await ethers.getContractFactory("MockV3Aggregator")
+  const feed = await Feed.deploy(DECIMALS, INITIAL_VALUE)
+  await feed.deployed()
+  console.log("Feed deployed to", feed.address)
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const Consumer = await ethers.getContractFactory("Consumer")
+  const consumer = await Consumer.deploy(feed.address)
+  await consumer.deployed()
+  console.log("Consumer deployed to", consumer.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
